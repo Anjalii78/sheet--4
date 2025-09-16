0a860958b95d385bf55922f77fee52ed3aa99c0b
~ANJALI~ <anjalikumari15243@gmail.com>
2025-09-16 04:12:26 -0500

    Update and rename averageq11.py to cubeofarray.py

---
+++ cubeofarray.py
@@ -0,0 +1,9 @@
+# you are provided with an integer array A. return another array B of size same as that of A
+#such that B[i]=A[i]^3
+A = [2, 6, 8, 1]
+B = []
+
+for i in range(len(A)):
+    B.append(A[i] ** 3)
+
+print(B)
