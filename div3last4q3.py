99e194545be75aad4e26d2479054690258b1a7f4
~ANJALI~ <anjalikumari15243@gmail.com>
2025-09-16 04:09:26 -0500

    Update and rename div3last4q3.py to maxandmin.py

--- div3last4q3.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter a number: "))
-last_digit = n - (10 * int(n / 10))
-if n % 3 == 0 and last_digit == 4:
-    print("Yes")
-else:
-    print("No")
---
+++ maxandmin.py
@@ -0,0 +1,12 @@
+#take input an array A of size N and write a program to print maximum and minimum 
+# elements of the input array.here N represent the length of the array
+
+    
+N = int(input())              
+A = list(map(int, input().split()))  
+
+print(max(A))   
+print(min(A))   
+    
+    
+    
